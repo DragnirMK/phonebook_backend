cea31aa7f45179a234dd5d39380b07fae00b74c2
DragnirMK <dragnirspeedrun@gmail.com>
2023-02-18 12:06:19 +0100

    Exercice 3.18

--- index.js
+++ index.js
@@ -6,35 +6,32 @@ const Person = require('./models/person')
 const app = express()
 
 app.use(express.static('build'))
-
 app.use(express.json())
 
 morgan.token('body', req => JSON.stringify(req.body || {}))
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use(cors())
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(p => {
-    if (p) {
-      response.json(p)
+app.get('/api/persons', (request, response, next) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+  .catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
     } else {
       response.status(404).end()
     }
   })
-  .catch(error => {
-    response.status(500).end()
-  })
+  .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
-})
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const person = request.body
   if (person.name === '' || person.number === '') {
     response.status(400).json({ 'error': 'name or number cannot be empty' }).end()
@@ -45,8 +42,9 @@ app.post('/api/persons', (request, response) => {
       response.status(400).json({ 'error': 'name already in phonebook' }).end()
     }
   })
+  .catch(error => next(error))
 
-  let newPerson = new Person({
+  const newPerson = new Person({
     name: person.name,
     number: person.number
   })
@@ -54,12 +52,33 @@ app.post('/api/persons', (request, response) => {
   newPerson.save().then(savedPerson => {
     response.json(newPerson)
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id).then(person => {
-    response.status(204).end()
+    if (person) {
+      response.status(204).end()
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const person = request.body
+
+  const newPerson = {
+    name: person.name,
+    number: person.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, newPerson, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -67,6 +86,30 @@ app.get('/info', (request, response) => {
   response.write("Phonebook has info for " + Person.length + " people. \n")
   response.end(Date())
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
